bdf6f14e19d5c8c418fc5c1bb83f44e97588d724
matthew <matthew.eichler@aventinesolutions.nl>
2014-01-29 13:19:39 +0100

    some production tekst

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Roelspits
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.hcj_link = "http://www.healthclubjordaan.nl/personal-trainer-roel-spits"
   end
 end
